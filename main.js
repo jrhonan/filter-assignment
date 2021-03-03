06379c8c37339450c5f3e94cdd8917932321e11f
jrhonan <70732788+jrhonan@users.noreply.github.com>
2021-03-03 15:16:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const select_menu = document.querySelector(".filter";)
+
+  select_menu.addEventListener("change", function(e){
+    const flex_container_class = e.target.value;
+    const active_items = document.querySelectorAll(".item.active");
+
+  active_items.forEach(function(item){
+    item.classList.remove("active");
+  });
+
+  const streaming_items = document.querySelectorAll(`.item.${flex_container_class}`);
+
+  streaming_items.forEach(function(item){
+    item.classList.add("active");
+  });
+});
+});
+});
